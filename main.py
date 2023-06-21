af5f3a5bb9d71eaf26799c13af29aa7123472d27
unknown <samratkiev@gmail.com>
2023-06-21 20:09:34 +0100

    different storeage option added

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import sys
+
 from storage_json import StorageJson
 from movie_app import MovieApp
 from storage_csv import StorageCsv
 def main():
-    storage = StorageJson('data.json')
-    movie_app = MovieApp(storage)
-    movie_app.run()
+
+    file_path = input("Enter  the storage type :")
+
+    if file_path == 'data.json':
+        storage = StorageJson('data.json')
+        movie_app = MovieApp(storage)
+        movie_app.run()
+    elif file_path == "data.csv":
+        storage = StorageCsv("data.csv")
+        movie_app = MovieApp(storage)
+        movie_app.run()
+    else:
+        print("file path does not exist")
+        sys.exit()
+
 
 
 if __name__ == "__main__":
